0b94968b51e224884ea8e3f773227803273b538c
Bishalsingh153 <bishalsinghofcp@gmail.com>
2026-09-14 11:27:39 +0530

    So this whole section's job: take a blob of text, clean up each word, and make one little (word, 1) box for every single word you see — no counting/adding yet, just tagging.

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func readInputFromUser() string {
 	fmt.Println("Enter text (type'END' on a new line to finish):")
 	scanner := bufio.NewScanner(os.Stdin)
 	var lines []string
-	for scanner.scan(){
+	for scanner.Scan(){
 		line := scanner.Text()
-		if line == "END":
+		
 		if line == "END" {
 			break
 		}
@@ -39,8 +39,28 @@ func splitIntoChunks(text string, chunkSize int) [] string {
 		if end > len(words) {
 			end = len(words)
 		}
-		chunks:= append(chunks, strings.Join(words[i:end]," "))
+		chunks = append(chunks, strings.Join(words[i:end]," "))
 	}
 	
 	return chunks
 }
+
+var punctuationRegex = regexp.MustCompile(`[^\w\s]`) //cleaning up messy words
+
+func normalize (word,string) string {
+	word = strings.ToLower(word)
+	word=punctuationRegex.ReplaceAllString(word,"")
+	return word
+}
+//The actual Map step
+func mapFunc(document string) []Pair {
+	var pairs []Pair
+	words:=strings.Fields(document)
+	for _,w := range words {
+		clean:=normalize(w)
+		if clean!=""{
+			pairs = append(pairs,Pair{Word: clean, Count:1})
+		}
+	} return pairs
+}
+
